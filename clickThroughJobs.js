f90344f63728a2e290540026a222114511712780
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-27 22:51:47 -0500

    Update clickThroughJobs.js

--- clickThroughJobs.js
+++ clickThroughJobs.js
@@ -1,21 +1,44 @@
+function checker(elm, type) {  if (elm != undefined) {    if (type == 'href') {      return elm.href;    }    if (type == 'text') {      return elm.innerText.trim();    }    if (type == 'next') {      return elm;    }  } else {    return '';  }}
 
-function getJobPostingObj(obj, n){
+var numberOfPagesToLoop = Math.ceil(parseInt(checker(document.getElementsByClassName('jobs-search-two-pane__alerts')[0].getElementsByTagName('div')[1],'text').replace(/\D+/g, ''))/50);
+
+function clickJobListItem() {
+  setTimeout(() => {
+    var jobList = document.getElementsByClassName(
+      'jobs-search-results__list artdeco-list artdeco-list--offset-4')[0].getElementsByClassName('artdeco-list__item');
+
+    function getJobPostingObj(obj, n) {
+      setTimeout(() => {
+        document.getElementById(obj.getAttribute('id')).scrollIntoView();
+
+        obj.getElementsByClassName('job-card-search--clickable')[0].click();
+      }, ((n + 1) * 2800));
+    }
+
+    for (i = 0; i < jobList.length; i++) {
+      getJobPostingObj(jobList[i], i);
+    }
+
+  }, 2900);
+}
+
+function clickNextPage() {
 setTimeout(()=>{
-
-	document.getElementById(obj.getAttribute('id')).scrollIntoView();
-
-	obj.getElementsByClassName('job-card-search--clickable')[0].click();
-},((n+1)*2000));
-}
-var jobList = document.getElementsByClassName(
-'jobs-search-results__list artdeco-list artdeco-list--offset-4')[0].getElementsByClassName('artdeco-list__item');
-
-
-function loopThroughJobs(){
-	for(i=0; i<jobList.length; i++){
-		getJobPostingObj(jobList[i], i);
-	}
+  var pager = document.getElementsByClassName('page-list')[0].getElementsByTagName('li');
+  for (p = 0; p < pager.length; p++) {
+    if (pager[p].getAttribute('class') == 'active') {
+      var pg = pager[(p + 1)];
+      if (pg != undefined) {
+        pg.getElementsByTagName('button')[0].click();
+      } else {
+        console.log('done');
+      }
+    }
+  }
+},(3000*25));
 }
 
+var first = new Promise(res=>{res(clickJobListItem());});
 
-loopThroughJobs();
+first.then(clickNextPage())
+//need a loop to run this promise until 'done'
