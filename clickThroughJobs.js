7432ef4cc6842bb8299843340167ffdba7797967
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-27 21:39:36 -0500

    Update clickThroughJobs.js

--- clickThroughJobs.js
+++ clickThroughJobs.js
@@ -1,17 +1,21 @@
 
 function getJobPostingObj(obj, n){
 setTimeout(()=>{
+
+	document.getElementById(obj.getAttribute('id')).scrollIntoView();
+
 	obj.getElementsByClassName('job-card-search--clickable')[0].click();
 },((n+1)*2000));
 }
 var jobList = document.getElementsByClassName(
 'jobs-search-results__list artdeco-list artdeco-list--offset-4')[0].getElementsByClassName('artdeco-list__item');
 
-for(i=0; i<jobList.length; i++){
-	getJobPostingObj(jobList[i], i);
+
+function loopThroughJobs(){
+	for(i=0; i<jobList.length; i++){
+		getJobPostingObj(jobList[i], i);
+	}
 }
 
 
-
-
-
+loopThroughJobs();
